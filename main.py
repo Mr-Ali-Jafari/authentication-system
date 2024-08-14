7ce77ff8376e9e1baeb4f213eed93e3b1bc7a5cf
Mr-Elliot-0 <riptt91@gmail.com>
2024-08-14 15:11:56 +0330

    fix bugs

--- main.py
+++ main.py
@@ -78,14 +78,19 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
 
 # end section
 
+@app.get("/")
+def index():
+    return {
+        'message':' hello if you developer go to http://127.0.0.1:8000/docs'
+    }
 
 @app.post("/permissions/", response_model=schemas.Permission)
 def create_permission(permission: schemas.PermissionCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name == 'create_permission' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=401,
-            detail='you don`t have permission for this ',
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     db_permission = models.Permission(name=permission.name)
     db.add(db_permission)
     db.commit()
@@ -94,22 +99,22 @@ def create_permission(permission: schemas.PermissionCreate, db: Session = Depend
 
 @app.get("/permissions/", response_model=List[schemas.Permission])
 def read_permissions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name == 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=401,
-            detail='you don`t have permission for this ',
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     
     permissions = db.query(models.Permission).offset(skip).limit(limit).all()
     return permissions
 
 @app.get("/permissions/{permission_id}", response_model=schemas.Permission)
 def read_permission(permission_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name == 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=401,
-            detail='you don`t have permission for this ',
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     permission = db.query(models.Permission).filter(models.Permission.id == permission_id).first()
     if permission is None:
         raise HTTPException(status_code=404, detail="Permission not found")
@@ -117,11 +122,11 @@ def read_permission(permission_id: int, db: Session = Depends(get_db), current_u
 
 @app.post("/roles/", response_model=schemas.Role)
 def create_role(role: schemas.RoleCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     db_role = models.Role(name=role.name)
     for permission_id in role.permission_ids:
         permission = db.query(models.Permission).filter(models.Permission.id == permission_id).first()
@@ -134,21 +139,21 @@ def create_role(role: schemas.RoleCreate, db: Session = Depends(get_db), current
 
 @app.get("/roles/", response_model=List[schemas.Role])
 def read_roles(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     roles = db.query(models.Role).offset(skip).limit(limit).all()
     return roles
 
 @app.get("/roles/{role_id}", response_model=schemas.Role)
 def read_role(role_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     role = db.query(models.Role).filter(models.Role.id == role_id).first()
     if role is None:
         raise HTTPException(status_code=404, detail="Role not found")
@@ -156,16 +161,25 @@ def read_role(role_id: int, db: Session = Depends(get_db), current_user: schemas
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Access denied: You do not have permission to view users'
         )
-    db_user = models.User(username=user.username, email=user.email, hashed_password=user.password)  # هش کردن پسورد در عمل توصیه می‌شود
+    
+    hashed_password = get_password_hash(user.password)
+    
+    db_user = models.User(
+        username=user.username, 
+        email=user.email, 
+        hashed_password=hashed_password  
+    )
+    
     for role_id in user.role_ids:
         role = db.query(models.Role).filter(models.Role.id == role_id).first()
         if role:
             db_user.roles.append(role)
+    
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -178,11 +192,11 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), current
 # Users list
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     
     try:
         users = db.query(models.User).offset(skip).limit(limit).all()
@@ -200,11 +214,11 @@ def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), cu
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -212,11 +226,11 @@ def read_user(user_id: int, db: Session = Depends(get_db), current_user: schemas
 
 @app.put("/users/{user_id}", response_model=schemas.User)
 def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    if any(role.name != 'is_superuser' for role in current_user.roles):
+    if not any(role.name == 'is_superuser' for role in current_user.roles):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Access denied: You do not have permission to view users'
-        )
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail='Access denied: You do not have permission to view users'
+    )
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
